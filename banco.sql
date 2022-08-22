b39e67fd1782aedd9469cdc410a15989e68b9e84
Helano Moreira <94863983+HMC2022@users.noreply.github.com>
2022-08-22 16:48:50 -0300

    Create banco.sql

---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dados (
+    IDCliente int,
+    NomeCliente varchar(50),
+    SobrenomeCliente varchar(50),
+    EnderecoCliente varchar(150),
+    CidadeCliente varchar(50),
+    Host varchar(50)
+);
